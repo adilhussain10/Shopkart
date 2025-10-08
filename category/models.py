9c9fbcca09afb60d67301516536b3a2b80235ddf
Adil Hussain <adilhussain8662@gmail.com>
2025-10-08 20:07:28 +0530

    Initial commit of Django project

--- category/models.py
+++ category/models.py
@@ -4,17 +4,15 @@ from django.urls import reverse
 class Category(models.Model):
     category_name=models.CharField(max_length=50,unique=True)
     slug=models.SlugField(max_length=100,unique=True)
-    description=models.TextField(max_length=225,blank=True)
+    description=models.TextField(max_length=255,blank=True)
    
     cat_image = models.ImageField(upload_to='photos/categories', blank=True)
 
     class Meta:
         db_table = ''
         managed = True
-        verbose_name = 'categories'
+        verbose_name = 'category'
         verbose_name_plural = 'categories'
-
-
 
 
 def get_url(self):
